12d949c5f20c745a3f9c2fe1f804c6d5ca1877f0
Julián Andrés Robayo Quintero <jar@udistrital.edu.co>
2025-06-09 00:27:36 -0500

    Configuración base de Datos

--- Programa.py
+++ Programa.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from db import db
+from Estudiante import Estudiante
 
 
 class Programa:
@@ -8,9 +10,14 @@ class Programa:
         self.app=Flask(__name__)
         self.app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///estudiantes.sqlite3"
         
+        # Agregar la db a la app
+        db.init_app(self.app)
+        
         self.app.add_url_rule('/nuevo', view_func=self.agregar)
         
         #Iniciar Servidor
+        with self.app.app_context():
+            db.create_all()
         self.app.run(debug=True)
     
     def agregar(self):
