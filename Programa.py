568027b08cdd0d2223b54f6255f50b33b545f4b0
Julián Andrés Robayo Quintero <jar@udistrital.edu.co>
2025-06-09 00:56:42 -0500

    Agregar y Mostrar datos

--- Programa.py
+++ Programa.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from db import db
 from Estudiante import Estudiante
 
@@ -13,14 +13,37 @@ class Programa:
         # Agregar la db a la app
         db.init_app(self.app)
         
-        self.app.add_url_rule('/nuevo', view_func=self.agregar)
+        self.app.add_url_rule('/', view_func=self.buscarTodos)
+        self.app.add_url_rule('/nuevo', view_func=self.agregar, methods=["GET","POST"])
         
         #Iniciar Servidor
         with self.app.app_context():
             db.create_all()
         self.app.run(debug=True)
     
+    def buscarTodos(self):
+        return "To Do: Tengo que buscar en los registrors de la tabla"
+    
     def agregar(self):
+        
+        #verificar si debe enviar formulario o procesar datos
+        
+        if request.method=="POST":
+            #Crear un objeto de la clase Estudiante con los valores del formulario
+            nombre=request.form['nombre']
+            email=request.form['email']
+            codigo=request.form['codigo']
+            
+            miEstudiante=Estudiante(nombre, email, codigo)
+            
+            # Guardar el objeto en la base de Datos
+            
+            db.session.add(miEstudiante)
+            db.session.commit()
+            
+            return redirect(url_for('buscarTodos'))
+            
+            
         return render_template('nuevoEstudiante.html')
     
     
